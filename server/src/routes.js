f21ad84a7abbbf095d718e5956bcb23680c00bf5
Kai Matsuda <vangogh500@gmail.com>
2017-02-24 17:46:30 +0700

    logic to resend verification token

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt')
 
+var EmailVerificationToken = require('./models/emailVerificationToken.js')
+
 module.exports = function(app, transporter) {
   app.post('/emailVerification', function(req,res) {
     console.log("Processing email verification...")
@@ -14,7 +16,6 @@ module.exports = function(app, transporter) {
       }
       else {
         console.log("\t Success! Creating new token...")
-        var EmailVerificationToken = require('./models/emailVerificationToken.js')
         var emailVerificationToken = new EmailVerificationToken({email: user.email, password: hash});
         emailVerificationToken.createVerificationToken(function (err, token) {
           if(err) {
@@ -42,6 +43,40 @@ module.exports = function(app, transporter) {
             })
           }
         })
+      }
+    })
+  })
+  app.get('/emailverificationtoken/:email', function(req,res) {
+    console.log("Request to resend verification token...")
+    EmailVerificationToken.findOne({email: req.params.email}, function(e, found) {
+      if(e) {
+        console.log("\t ERROR: finding token")
+        console.log(e)
+      }
+      else if(found) {
+        console.log("\t Found token! Resending...")
+        var link = req.protocol + "://" + req.get('host') + "/verifyEmail/" + found.token;
+        var mailOptions = {
+          from: "UniLoL <uniloltemp@gmail.com>", // sender address
+          to: found.email, // list of receivers
+          subject: "Verification Link", // Subject line
+          text: "Test", // plaintext body
+          html: '<div style="background: #212121; color: #FFF; padding: 5px;"><h3>UniLol</h3></div><p>Thank you for registering with us. Follow the link below to get one step closer to participating in your campus community:</p><a href=\"' + link + '\" >' + link + '</a>' // html body
+        };
+        transporter.sendMail(mailOptions, function(err) {
+          if(err) {
+            console.log("\t ERROR: sending verification email")
+            console.log(err)
+          }
+          else {
+            console.log("\t Success!")
+            res.send()
+          }
+        })
+      }
+      else {
+        console.log("\t Token associated with email not found")
+        res.status(404).send()
       }
     })
   })
